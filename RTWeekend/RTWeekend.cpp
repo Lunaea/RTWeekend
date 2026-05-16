747d58b4a69a7b071dbc2cec33ebd46888340f91
Lunaea <132797043+Lunaea@users.noreply.github.com>
2026-05-15 19:35:53 -0700

    Add project files.

---
+++ RTWeekend/RTWeekend.cpp
@@ -0,0 +1,54 @@
+// RTWeekend.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include "color.h"
+#include "vec3.h"
+
+#include <iostream>
+#include <fstream>
+
+int main()
+{
+    // Filestream
+    
+    std::ofstream outf{ "image.ppm" };
+
+    if (!outf)
+    {
+        std::cerr << "Error writing to image.ppm\n";
+        return 1;
+    }
+
+    // Image
+
+    int image_width{ 256 };
+    int image_height{ 256 };
+
+    // Render
+
+    outf << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+    for (int j = 0; j < image_height; ++j)
+    {
+        std::clog << "\rScanlines remaining: " << (image_height - j) << std::flush;
+        for (int i = 0; i < image_width; ++i)
+        {
+            auto pixel_color = color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0);
+            write_color(outf, pixel_color);
+        }
+    }
+
+    std::clog << "\rFinished writing image.ppm\n";
+
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
