6c69e14889ec62ab693a5486a36ba6dfb64c94b4
Lunaea <132797043+Lunaea@users.noreply.github.com>
2026-05-22 14:06:03 -0700

    Implement camera class and refactor main

--- RTWeekend/RTWeekend.cpp
+++ RTWeekend/RTWeekend.cpp
@@ -1,22 +1,10 @@
 // RTWeekend.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 #include "rtweekend.h"
+#include "camera.h"
 #include "hittable.h"
 #include "hittable_list.h"
 #include "sphere.h"
-
-#include <fstream>
-
-color ray_color(const ray& r, const hittable& world)
-{
-    hit_record rec{};
-    if (world.hit(r, interval(0, infinity), rec))
-        return 0.5 * (rec.normal + color(1, 1, 1));
-
-    vec3 unit_direction{ unit_vector(r.direction()) };
-    auto a{ 0.5 * (unit_direction.y() + 1.0) };
-    return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
-}
 
 int main()
 {
@@ -30,15 +18,6 @@ int main()
         return 1;
     }
 
-    // Image
-
-    auto aspect_ratio = 16.0 / 9.0;
-    int image_width{400};
-
-    // Calculate the image height, and ensure that it's at least 1.
-    int image_height = int(image_width / aspect_ratio);
-    image_height = (image_height <1) ? 1 : image_height;
-
     // World
     
     hittable_list world{};
@@ -46,56 +25,11 @@ int main()
     world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
     world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
 
-    // Camera
+    camera cam{};
+    cam.aspect_ratio = 16.0 / 9.0;
+    cam.image_width = 400;
 
-    auto focal_length{ 1.0 };
-    auto viewport_height{ 2.0 };
-    auto viewport_width = viewport_height * (double(image_width) / image_height);
-    auto camera_center{ point3(0,0,0) };
-
-    // Calculate the vectors across the horizontal and down the vertical viewport edges.
-    auto viewport_u{ vec3(viewport_width, 0, 0) };
-    auto viewport_v{ vec3(0, -viewport_height, 0) };
-
-    // Calculate the horizontal and vertical delta vectors from pixel to pixel
-    auto pixel_delta_u{ viewport_u / image_width };
-    auto pixel_delta_v{ viewport_v / image_height };
-
-    // Calculate the location of the upper left pixel.
-    auto viewport_upper_left = camera_center
-                             - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
-    auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
-
-    // Render
-
-    outf << "P3\n" << image_width << ' ' << image_height << "\n255\n";
-
-    for (int j = 0; j < image_height; ++j)
-    {
-        std::clog << "\rScanlines remaining: " << (image_height - j) << std::flush;
-        for (int i = 0; i < image_width; ++i)
-        {
-            auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
-            auto ray_direction = pixel_center - camera_center;
-            ray r(camera_center, ray_direction);
-
-            color pixel_color{ ray_color(r, world) };
-            write_color(outf, pixel_color);
-        }
-    }
-
-    std::clog << "\rFinished writing image.ppm\n";
+    cam.render(world, outf);
 
     return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
